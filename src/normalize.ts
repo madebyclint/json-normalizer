ecec011bb379c7bebb738d33a7907d01f78c8c87
Clint Bush <clint.bush@pikestreetlabs.com>
2022-09-20 11:19:05 -0700

    Boilerplate for simple utility

---
+++ src/normalize.ts
@@ -0,0 +1,11 @@
+const logger = require('node-color-log');
+logger.setLevel('success');
+
+// shows everytime
+logger.log('hello world [LOG]');
+// respects log level
+logger.success('hello world [SUCCESS]');
+logger.debug('hello world [DEBUG]');
+logger.info('hello world [INFO]');
+logger.warn('hello world [WARN]');
+logger.error('hello world [ERROR]');
